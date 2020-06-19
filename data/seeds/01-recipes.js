cbff34c7bba531fa947d2b0198606d5f5f94db75
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-18 23:03:48 -0400

    Changed seed and migration functions to async

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,15 +1,10 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('recipe-book').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('recipe-book').insert([
-         {name: 'recipe-1'},
-         {name: 'recipe-2'},
-         {name: 'recipe-3'},
-         {name: 'recipe-4'},
-         {name: 'recipe-5'}
-      ]);
-    });
-};
+exports.seed = async function(knex) {
+	await knex('recipes').insert([
+    {name: 'recipe-1'},
+    {name: 'recipe-2'},
+    {name: 'recipe-3'},
+    {name: 'recipe-4'},
+    {name: 'recipe-5'}
+  ])
+}
