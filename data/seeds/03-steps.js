880eaa8c0f63de6f6e7d8429461a669a6ef504e3
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-27 15:25:17 -0400

    Update 03-steps.js

--- data/seeds/03-steps.js
+++ data/seeds/03-steps.js
@@ -3,19 +3,19 @@
 exports.seed = async function(knex) {
 	await knex('steps').insert([
     { recipe_id: 1, step_number: 1, ingredient_id: 1, quantity: 2, description: 'Add' },
-    { recipe_id: 1, step_number: 2, ingredient_id: 3, quantity: 5, description: 'Add' },
-    { recipe_id: 1, step_number: 3, ingredient_id: 1, quantity: 3, description: 'Add' },
+    { recipe_id: 1, step_number: 2, ingredient_id: 3, quantity: 5, description: 'Stir' },
+    { recipe_id: 1, step_number: 3, ingredient_id: 1, quantity: 3, description: 'Cook' },
     { recipe_id: 2, step_number: 1, ingredient_id: 5, quantity: 7, description: 'Add' },
-    { recipe_id: 2, step_number: 2, ingredient_id: 1, quantity: 4, description: 'Add' },
-    { recipe_id: 2, step_number: 3, ingredient_id: 4, quantity: 1, description: 'Add' },
+    { recipe_id: 2, step_number: 2, ingredient_id: 1, quantity: 4, description: 'Remove' },
+    { recipe_id: 2, step_number: 3, ingredient_id: 4, quantity: 1, description: 'Stir' },
     { recipe_id: 3, step_number: 1, ingredient_id: 2, quantity: 6, description: 'Add' },
-    { recipe_id: 3, step_number: 2, ingredient_id: 1, quantity: 4, description: 'Add' },
-    { recipe_id: 3, step_number: 3, ingredient_id: 4, quantity: 2, description: 'Add' },
+    { recipe_id: 3, step_number: 2, ingredient_id: 1, quantity: 4, description: 'Stir' },
+    { recipe_id: 3, step_number: 3, ingredient_id: 4, quantity: 2, description: 'Cook' },
     { recipe_id: 4, step_number: 1, ingredient_id: 1, quantity: 1, description: 'Add' },
-    { recipe_id: 4, step_number: 2, ingredient_id: 3, quantity: 2, description: 'Add' },
-    { recipe_id: 4, step_number: 3, ingredient_id: 5, quantity: 6, description: 'Add' },
-    { recipe_id: 5, step_number: 1, ingredient_id: 2, quantity: 2, description: 'Add' },
-    { recipe_id: 5, step_number: 2, ingredient_id: 1, quantity: 1, description: 'Add' },
+    { recipe_id: 4, step_number: 2, ingredient_id: 3, quantity: 2, description: 'Remove' },
+    { recipe_id: 4, step_number: 3, ingredient_id: 5, quantity: 6, description: 'Stir' },
+    { recipe_id: 5, step_number: 1, ingredient_id: 2, quantity: 2, description: 'Cook' },
+    { recipe_id: 5, step_number: 2, ingredient_id: 1, quantity: 1, description: 'Remove' },
     { recipe_id: 5, step_number: 3, ingredient_id: 4, quantity: 3, description: 'Add' }
   ])
 }
